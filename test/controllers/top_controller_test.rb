9bafe9a39115cc0802193a3d7c94c159b8923a72
yoheitaonishi <onishiyoheita@gmail.com>
2026-01-19 18:08:12 +0900

    テスト修正

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TopControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get top_index_url
+    get root_url
     assert_response :success
   end
 

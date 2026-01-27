03e102a7d52981d85390cbbb73b0c017854c0457
naoyanickf <naoyanickf@gmail.com>
2026-01-27 19:16:33 +0900

    timezone tokyo

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,10 @@ module DataAgent
 
     # Autoload services directory
     config.autoload_paths << Rails.root.join("app/services")
+
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
   end
 end
